87e79f14d82023fecc13a456bf213f1ebd6f950e
HaarikaR <hramadugu@gmail.com>
2022-04-02 21:15:40 -0700

    active record for grouping favorites based on place_id

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -6,6 +6,8 @@ end
 validates :things_to_do_id, uniqueness: { scope: :user_id, :message => "has already been added as favorite" }
 
 def group_by_favorite
-Favorite.group(:things_to_do_id).count
+ Favorite.joins(:things_to_do).group(:place_id).count(:user_id)
 end
+
+
 
