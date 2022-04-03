8668e96e2ce40faceee43d710e3f0455ca584736
HaarikaR <hramadugu@gmail.com>
2022-04-02 20:02:01 -0700

    added validation for already added things_to_do

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,3 +2,10 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :things_to_do
 end
+
+validates :things_to_do_id, uniqueness: { scope: :user_id, :message => "has already been added as favorite" }
+
+def group_by_favorite
+Favorite.group(:things_to_do_id).count
+end
+
