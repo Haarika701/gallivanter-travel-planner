8f264c5713a2342b99dd76409ef6cf20d259dbd8
HaarikaR <hramadugu@gmail.com>
2022-04-06 20:58:55 -0700

    feat:functionality added for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,21 @@
 class PlacesController < ApplicationController
 
     def index
-        places = Place.all 
-        #place = Place.first
+        places_rel = Place.all 
+        places = places_rel.to_a
         @places_render = places[0].top_places()
-        render json: places
+        placeid_list, favnum = @places_render.transpose
+        fav_places = []
+        placeid_list.each {|placeid|
+        places.delete_if do |place|
+            if place.id == placeid 
+                fav_places.push(place)
+                true
+            end 
+        end
+        }
+        fav_places.push(*places)
+        render json: fav_places
     end
 
     def show 
